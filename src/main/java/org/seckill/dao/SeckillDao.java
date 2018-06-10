97d257377fcb553dd4365567d6a3cc4b40989da2
cayden <1406142870@qq.com>
2018-06-11 00:31:29 +0800

    done

--- src/main/java/org/seckill/dao/SeckillDao.java
+++ src/main/java/org/seckill/dao/SeckillDao.java
@@ -1,5 +1,6 @@
 package org.seckill.dao;
 
+import org.apache.ibatis.annotations.Param;
 import org.seckill.entity.Seckill;
 
 import java.util.Date;
@@ -13,7 +14,7 @@ public interface SeckillDao {
     * @param killTime
     * @return 如果影响行数大于等于1，表示更新的记录行数
     * */
-    int reduceNumber(long seckillId, Date killTime);
+    int reduceNumber(@Param("seckillId") long seckillId, @Param("killTime") Date killTime);
 
     /**
      * 根据id查询秒杀对象
@@ -28,6 +29,6 @@ public interface SeckillDao {
      * @param limit
      * @return
      * */
-    List<Seckill> queryAll(int offset, int limit);
+    List<Seckill> queryAll(@Param("offset") long offset, @Param("limit") long limit);
 
 }
