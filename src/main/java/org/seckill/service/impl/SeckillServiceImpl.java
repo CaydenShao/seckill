85f9009d259d3e6a962c71e4d7d06b9305572e87
cayden <1406142870@qq.com>
2018-06-13 00:20:50 +0800

    添加注解控制事务方法

--- src/main/java/org/seckill/service/impl/SeckillServiceImpl.java
+++ src/main/java/org/seckill/service/impl/SeckillServiceImpl.java
@@ -13,17 +13,24 @@ import org.seckill.exception.SeckillException;
 import org.seckill.service.SeckillService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.DigestUtils;
 
 import java.util.Date;
 import java.util.List;
-
+// @Component @Service @Dao @Controller
+@Service
 public class SeckillServiceImpl implements SeckillService {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    // 注入Service依赖
+    @Autowired
     private SeckillDao seckillDao;
 
+    @Autowired
     private SuccessKilledDao successKilledDao;
 
     private final String salt = "q5qwt345rewtaera3w5344352";
@@ -39,6 +46,13 @@ public class SeckillServiceImpl implements SeckillService {
     }
 
     @Override
+    @Transactional
+    /**
+     * 使用注解控制事务方法的优点：
+     * 1.开发团队达成一致约定，明确标注事务方法的编程风格。
+     * 2.保证事务方法的执行时间尽可能短，不要穿插其他网络操作RPC/HTTP请求或者剥离到事务方法外部。
+     * 3.不是所有的方法都需要事务，如只有一条修改操作，只读操作不需要事务控制。
+     * */
     public Exposer exportSeckillUrl(long seckillId){
         Seckill seckill = this.seckillDao.queryById(seckillId);
         if(seckill == null){
